caf65b965ad67aae2c86bb5c9e93b7bb58e49ef7
Alex Boisseau <alex.boisseau79@gmail.com>
2022-02-13 15:46:56 +0100

    feat(app & header): add react-router-dom and create header
    add react-router-dom and create three routes inside the App component. Create also an Header
    component with Link to access differents pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
 import { FC } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Header from './components/Header/Header';
 
 const App: FC = () => {
-  return <h1 className="text-3xl font-bold underline text-red-500">WebFlix</h1>;
+  return (
+    <main className="w-full min-h-screen bg-zinc-900 text-white">
+      <BrowserRouter>
+        <Header />
+        <div className="2xl:px-86 xl:px-60 lg:px-40 md:px-28 sm:px-20 px-5">
+          <Routes>
+            <Route path="/" element={<div>Home Page</div>} />
+            <Route path="/movies" element={<div>Movies Page 📽</div>} />
+            <Route path="/movies/:id" element={<div>Detail Movie Page</div>} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </main>
+  );
 };
 
 export default App;
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,21 @@
+import { FC } from 'react';
+import { Link } from 'react-router-dom';
+
+const Header: FC = () => {
+  return (
+    <header className="sticky top-0 bg-gradient-to-t from-zinc-900 to-black flex justify-between items-center sm:py-10 py-6 2xl:px-86 xl:px-60 lg:px-40 md:px-28 sm:px-20 px-5 mb-5">
+      <Link to="/">
+        <p className="font-bold sm:text-3xl text-2xl">WebFlix 🍿</p>
+      </Link>
+      <nav>
+        <Link to="/movies">
+          <p className="text-gray-300 hover:text-white hover:underline">
+            Movies
+          </p>
+        </Link>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
