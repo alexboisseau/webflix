16162a8bd5fc288b201350124e2180a1f1b0d770
Alex Boisseau <alex.boisseau79@gmail.com>
2022-02-13 16:11:57 +0100

    feat(app): create Footer and Home component and adjust UI of App and Header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,27 @@
 import { FC } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
+import Home from './components/Home/Home';
 
 const App: FC = () => {
   return (
-    <main className="w-full min-h-screen bg-zinc-900 text-white">
+    <main className="flex flex-col justify-between w-full min-h-screen bg-zinc-900 text-white">
       <BrowserRouter>
-        <Header />
-        <div className="2xl:px-86 xl:px-60 lg:px-40 md:px-28 sm:px-20 px-5">
-          <Routes>
-            <Route path="/" element={<div>Home Page</div>} />
-            <Route path="/movies" element={<div>Movies Page 📽</div>} />
-            <Route path="/movies/:id" element={<div>Detail Movie Page</div>} />
-          </Routes>
+        <div>
+          <Header />
+          <div className="2xl:px-86 xl:px-60 lg:px-40 md:px-28 sm:px-20 px-5">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/movies" element={<div>Movies Page 📽</div>} />
+              <Route
+                path="/movies/:id"
+                element={<div>Detail Movie Page</div>}
+              />
+            </Routes>
+          </div>
         </div>
+        <Footer />
       </BrowserRouter>
     </main>
   );
---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react';
+
+const Footer: FC = () => {
+  return (
+    <p className="text-gray-500 text-center py-5 2xl:px-86 xl:px-60 lg:px-40 md:px-28 sm:px-20 px-5">
+      Developped by Alex Boisseau during React course at Bordeaux Ynov Campus
+    </p>
+  );
+};
+
+export default Footer;
