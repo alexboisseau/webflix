1a81ae830b0dbeacec4f8d05f8b4a13d3e181349
Alex Boisseau <alex.boisseau79@gmail.com>
2022-02-22 07:24:44 +0100

    fix(favoriteslice): change type of initialState

--- src/store/favoriteSlice.ts
+++ src/store/favoriteSlice.ts
@@ -1,6 +1,6 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-const favoritesMovies: string[] = [];
+const favoritesMovies: number[] = [];
 
 const favoriteSlice = createSlice({
   name: 'favorite',
